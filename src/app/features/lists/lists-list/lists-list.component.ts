7d5524fe24b6daa24246cefdda9129a8d01dcf04
Túlio Santos <tuliosantos.contato@gmail.com>
2026-06-15 19:19:36 -0300

    [wip] feat: creating lists components and routes

---
+++ src/app/features/lists/lists-list/lists-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lists-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './lists-list.component.html',
+  styleUrl: './lists-list.component.scss'
+})
+export class ListsListComponent {
+
+}
---
+++ src/app/features/lists/lists.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-lists',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './lists.component.html',
+  styleUrl: './lists.component.scss'
+})
+export class ListsComponent {
+
+}
